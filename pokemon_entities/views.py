e3896844eb5ef39512fca917a1ed63cc9b27c3c1
Katerina <e.mayakovskaya@gmail.com>
2025-07-03 20:39:31 +0300

    Add evolution display to pokemon page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -62,10 +62,11 @@ def show_pokemon(request, pokemon_id):
     requested_pokemon = get_object_or_404(Pokemon, id=pokemon_id)
     current_time = localtime(now())
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+
     for entity in PokemonEntity.objects.filter(
-        pokemon=requested_pokemon,
-        appeared_at__lte=current_time,
-        disappeared_at__gte=current_time
+            pokemon=requested_pokemon,
+            appeared_at__lte=current_time,
+            disappeared_at__gte=current_time
     ):
         add_pokemon(
             folium_map, entity.lat,
@@ -75,15 +76,31 @@ def show_pokemon(request, pokemon_id):
 
     pokemon_on_page = {
         'pokemon_id': requested_pokemon.id,
-        'img_url': request.build_absolute_uri(requested_pokemon.image.url) if requested_pokemon.image else DEFAULT_IMAGE_URL,
+        'img_url': request.build_absolute_uri(
+            requested_pokemon.image.url) if requested_pokemon.image else DEFAULT_IMAGE_URL,
         'title_ru': requested_pokemon.title,
         'title_en': requested_pokemon.title_en,
         'title_jp': requested_pokemon.title_jp,
         'description': requested_pokemon.description,
     }
 
+    if requested_pokemon.evolve_to:
+        next_pokemon = requested_pokemon.evolve_to
+        pokemon_on_page['next_evolution'] = {
+            'title_ru': next_pokemon.title,
+            'pokemon_id': next_pokemon.id,
+            'img_url': request.build_absolute_uri(next_pokemon.image.url) if next_pokemon.image else DEFAULT_IMAGE_URL
+        }
+
+    if requested_pokemon.evolve_from:
+        prev_pokemon = requested_pokemon.evolve_from
+        pokemon_on_page['previous_evolution'] = {
+            'title_ru': prev_pokemon.title,
+            'pokemon_id': prev_pokemon.id,
+            'img_url': request.build_absolute_uri(prev_pokemon.image.url) if prev_pokemon.image else DEFAULT_IMAGE_URL
+        }
+
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(),
         'pokemon': pokemon_on_page,
     })
-
