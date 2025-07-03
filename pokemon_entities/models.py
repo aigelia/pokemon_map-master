081d96a0f8492204503b47030652f0a1b2416d64
Katerina <e.mayakovskaya@gmail.com>
2025-07-03 21:10:08 +0300

    Update required constraints for models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,11 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=50, null=True)
-    title_en = models.CharField(max_length=50, null=True)
-    title_jp = models.CharField(max_length=50, null=True)
-    image = models.ImageField(upload_to='pokemon_images', null=True, blank=True)
-    description = models.TextField(null=True)
+    title = models.CharField(max_length=50, null=False, default='Неизвестный покемон')
+    title_en = models.CharField(max_length=50, null=True, blank=True)
+    title_jp = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField(upload_to='pokemon_images', null=True)
+    description = models.TextField(null=True, blank=True)
     previous_evolution = models.ForeignKey(
         'self',
         null=True,
@@ -25,12 +25,12 @@ class PokemonEntity(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField(null=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    disappeared_at = models.DateTimeField(null=True)
+    level = models.IntegerField(null=True, blank=True)
+    health = models.IntegerField(null=True, blank=True)
+    strength = models.IntegerField(null=True, blank=True)
+    defence = models.IntegerField(null=True, blank=True)
+    stamina = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.pokemon}'
