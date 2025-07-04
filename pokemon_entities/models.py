2201df72cae8833afa0f855cd4e9f85f669a6c9f
Katerina <e.mayakovskaya@gmail.com>
2025-07-04 23:17:38 +0300

    Add related_name for pokemon in PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -21,7 +21,11 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        related_name='entities'
+    )
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField(null=True)
